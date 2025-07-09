07ffdf683c570998a5faa9f3ab4c1f522ad64ab8
malihabhatti <malihabhatti139@gmail.com>
2025-07-10 02:25:25 +0500

    Update index.jsx

--- app/index.jsx
+++ app/index.jsx
@@ -5,7 +5,7 @@ export default function Index() {
   return (
     <View
       className={
-        " flex justify-center items-center bg-[#f49b33] w-full h-full"
+        "m-2 flex justify-center items-center bg-[#f49b33] w-full h-full"
       }
     >
       <Text>This is our App Explore it</Text>
