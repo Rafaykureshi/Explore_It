51d5862315be7bd8de36cb68166d28cd51c8ced6
Rafaykureshi <a.rafayqureshi100@gmail.com>
2025-07-10 01:49:01 +0500

    req

--- app/index.jsx
+++ app/index.jsx
@@ -1,16 +1,11 @@
-import { Text, View } from "react-native";
+import { Text, View, ScrollView } from "react-native";
+import "../global.css";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-      className="bg-red-600"
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+        <View className={"m-2 flex justify-center items-center bg-red-600"}>
+          <Text>This is our App</Text>
+          </View>
   );
 }
