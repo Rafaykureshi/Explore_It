da59ce5666f418f086804d18fb957c5e25925022
Rafaykureshi <a.rafayqureshi100@gmail.com>
2025-07-10 02:10:44 +0500

    Update index.jsx

--- app/index.jsx
+++ app/index.jsx
@@ -1,11 +1,14 @@
-import { Text, View, ScrollView } from "react-native";
+import { Text, View } from "react-native";
 import "../global.css";
-import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-        <View className={"m-2 flex justify-center items-center bg-red-600 w-full h-full"}>
-          <Text>This is our App</Text>
-          </View>
+    <View
+      className={
+        "m-2 flex justify-center items-center bg-red-600 w-full h-full"
+      }
+    >
+      <Text>This is our App Explore it</Text>
+    </View>
   );
 }
