4bc6d7d8a3d311624a16ce42a33e6db50e6e6ef7
Rafaykureshi <a.rafayqureshi100@gmail.com>
2025-07-10 02:14:05 +0500

    Update index.jsx

--- app/index.jsx
+++ app/index.jsx
@@ -1,28 +1,14 @@
 import { Text, View } from "react-native";
-<<<<<<< HEAD
-=======
 import "../global.css";
->>>>>>> da59ce5666f418f086804d18fb957c5e25925022
 
 export default function Index() {
   return (
     <View
-<<<<<<< HEAD
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-     
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-=======
       className={
         "m-2 flex justify-center items-center bg-red-600 w-full h-full"
       }
     >
       <Text>This is our App Explore it</Text>
->>>>>>> da59ce5666f418f086804d18fb957c5e25925022
     </View>
   );
 }
